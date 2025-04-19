fe79a0f1f785afc3d68e5e20f63a63e419b71cd0
marko220991 <19marko91.ilic@gmail.com>
2025-04-19 08:58:30 +0200

    add Enterprise bean to the Pricing application

---
+++ src/main/java/pricing/business/PricingEnterpriseBean.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB40/StatelessEjbClass.java to edit this template
+ */
+package pricing.business;
+
+import jakarta.ejb.Stateless;
+import jakarta.ejb.LocalBean;
+
+/**
+ *
+ * @author Hp
+ */
+@Stateless
+@LocalBean
+public class PricingEnterpriseBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
