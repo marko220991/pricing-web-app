4da5a444a88330a6efb8082f4bb1904ff36d3205
marko220991 <19marko91.ilic@gmail.com>
2025-04-21 11:26:13 +0200

    add business methods to enterprise bean

--- src/main/java/pricing/business/PricingEnterpriseBean.java
+++ src/main/java/pricing/business/PricingEnterpriseBean.java
@@ -5,16 +5,65 @@
 package pricing.business;
 
 import jakarta.ejb.Stateless;
-import jakarta.ejb.LocalBean;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.Calendar;
+import java.util.List;
+import pricing.entity.Price;
 
 /**
  *
  * @author Hp
  */
 @Stateless
-@LocalBean
 public class PricingEnterpriseBean {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceContext
+    private EntityManager em;
+    
+    private Double getLastPrice(String itemCode) {
+        String stringQuery = "SELECT p.price FROM Price p WHERE p.itemCode = ? ORDER BY "
+                + "p.date DESC FETCH FIRST 1 ROWS ONLY";
+        
+        Query q = em.createNativeQuery(stringQuery);
+        q.setParameter(1, itemCode);
+        
+        return (Double) q.getSingleResult();
+    }
+    
+    private List<Price> getLastTwoPrices(String itemCode) {
+        String stringQuery = "SELECT p.id, p.itemCode, p.price, p.date FROM Price p WHERE p.itemCode = ? ORDER BY "
+                + "p.date DESC FETCH FIRST 2 ROWS ONLY";
+        
+        Query q = em.createNativeQuery(stringQuery);
+        q.setParameter(1, itemCode);
+        
+        return (List<Price>) q.getSingleResult();
+    }
+    
+    public void saveLastPriceChange(String itemCode, Double newPrice) {
+        Price price = new Price(itemCode, newPrice, Calendar.getInstance()); 
+        em.persist(price);
+    }
+    
+    public void applyPriceChange(String itemCode, int priceChange) {
+        Double lastPrice = getLastPrice(itemCode);
+        
+        Double newPrice = lastPrice + (priceChange);
+        saveLastPriceChange(itemCode, newPrice);
+    }
+    
+    public Double[] getOldAndNewPrices(String itemCode) {
+        Double[] prices = null;
+        
+        List<Price> lastTwoPrices = getLastTwoPrices(itemCode);
+        if (lastTwoPrices != null && lastTwoPrices.size() == 2) {
+            prices = new Double[2];
+            prices[0] = lastTwoPrices.get(1).getPrice();
+            prices[1] = lastTwoPrices.get(2).getPrice();
+        }
+        
+        return prices;
+    }
 }
